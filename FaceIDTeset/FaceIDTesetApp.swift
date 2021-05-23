7cd672cfe85131d5092cfebcb760e25d78702f03
Gagoil <cplkimdh@gmail.com>
2021-05-23 14:55:26 +0900

    Initial Commit

---
+++ FaceIDTeset/FaceIDTesetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDTesetApp.swift
+//  FaceIDTeset
+//
+//  Created by Donghyun Kim on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDTesetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
